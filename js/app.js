2078efd6289e2eaabca392b47807c207ec484667
Tzu-Chao Wang <john29917958@gmail.com>
2022-06-12 23:21:52 +0800

    Change canvas to white and add colors

--- js/app.js
+++ js/app.js
@@ -10,9 +10,37 @@ M.AutoInit();
     const foreColorPanel = document.querySelector('#fore-color-panel');
     let colorCharts = [];
     var colors = [
-        '#f44336', // red
-        '#4caf50', // green
-        '#03a9f4' // blue
+        '#000000', // black
+        '#FFFFFF', // white
+        '#0C0C0C',
+        '#191919',
+        '#262626',
+        '#333333',
+        '#4C4C4C',
+        '#666666',
+        '#7F7F7F',
+        '#999999',
+        '#B3B3B3',
+        '#CCCCCC',
+        '#D9D9D9',
+        '#E6E6E6',
+        '#F3F3F3',
+        '#FF0000', // red
+        '#FFFF00', // yellow
+        '#00FF00', // green
+        '#00FFFF', // cyan
+        '#0000FF', // blue
+        '#FF00FF',
+        '#2A2C30',
+        '#464B55',
+        '#798BC8',
+        '#A7B0C8',
+        '#C8CFE4',
+        '#36322D',
+        '#56493D',
+        '#6B5745',
+        '#B58F7B',
+        '#C0A292'
     ];
 
     function getCursorPosition(canvas, e) {
@@ -37,8 +65,8 @@ M.AutoInit();
         canvasCtx.strokeStyle = color;
     }
 
-    canvasCtx.canvas.width = document.body.offsetWidth - (sideToolBar.offsetWidth + sidebar.offsetWidth + 50);
-    canvasCtx.canvas.height = document.body.offsetHeight;    
+    canvasCtx.canvas.width = document.body.offsetWidth - (sideToolBar.offsetWidth + sidebar.offsetWidth + 30);
+    canvasCtx.canvas.height = document.body.offsetHeight - 68;    
 
     foreColorPanel.style.backgroundColor = colors[0];
     colors.forEach(function (color, index) {
